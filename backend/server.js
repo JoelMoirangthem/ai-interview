bbc89b1d7ea55f3781acb740f182c0cb0beb1aaa
JoelMoirangthem <joelmoirang@gmail.com>
2026-06-04 22:08:07 +0530

    add Render deployment config

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,9 @@ connectDB();
 setInterval(interviewState.sweep, 5 * 60 * 1000).unref();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173'],
+  origin: process.env.CORS_ORIGIN
+    ? process.env.CORS_ORIGIN.split(',')
+    : ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173'],
   credentials: true
 }));
 app.use(express.json({ limit: '10mb' }));
